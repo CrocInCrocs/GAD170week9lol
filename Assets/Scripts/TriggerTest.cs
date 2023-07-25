4b8f3b7b680d21db93c97608af015263ac6df30e
CrocInCrocs <ethanmoods13@outlook.com>
2023-07-25 12:12:48 +1000

    oh my gah

---
+++ Assets/Scripts/TriggerTest.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("'you kiss ur mother with that mouth?' Tyler 'Ninja' Blevins 2017");
+    }
+}
